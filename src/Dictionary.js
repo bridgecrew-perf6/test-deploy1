8b0164fa2bb77f725d53117a0c9e933e49ae38a0
anatick <91561543+anatick@users.noreply.github.com>
2022-04-22 17:10:59 +0100

    Added synonyms

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,9 +1,7 @@
 import React, {useState} from "react"; 
 import axios from "axios";
 import Results from "./Results";
-
 import "./Dictionary.css"
-
 
 export default function Dictionary (){
   let [keyword, setKeyword] = useState("");
@@ -13,38 +11,28 @@ export default function Dictionary (){
     setResults(response.data[0]);
   }
 
-
   function search(event){
     event.preventDefault();
 
     // documentation: https://dictionaryapi.dev/
-
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleResponse)
   }
-
-  
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
 
-
   return (
     <div className="Dictionary">
       <form onSubmit={search}>
-        <div className="row">
-          <div className="col-8">
-            <input 
-            className="WordSearch"
-            type="search" onChange={handleKeywordChange}
-            />
-            <Results results={results} />
-          </div>
-
-        </div>
+        <input 
+        className="WordSearch"
+        type="search" 
+        onChange={handleKeywordChange}
+        />
+        <Results results={results} />
       </form>
-      
     </div>
   );
 }
