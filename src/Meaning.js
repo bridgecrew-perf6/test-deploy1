80a9ae46c6cc3a90cd52363db55ac93f1a0f26de
anatick <91561543+anatick@users.noreply.github.com>
2022-04-27 15:26:13 +0100

    Added phonetics to project

--- src/Meaning.js
+++ src/Meaning.js
@@ -2,23 +2,22 @@ import React from "react";
 import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
-  console.log(props.meaning)
   return (
     <div className="Meaning">
-      <h3>{props.meaning.partOfSpeech}</h3>
-      {props.meaning.definitions.map(function(definition, index) { 
-        return (
-          <div key={index}> 
-            <p>
-              <strong>Definition: </strong>{definition.definition}
-              <br />
-              <em>{definition.example}</em>
-              <br />
-              <Synonyms synonyms ={definition.synonyms} />
-            </p>
-          </div>
-        );
-      })}
+        <h3>{props.meaning.partOfSpeech}</h3>
+        {props.meaning.definitions.map(function(definition, index) { 
+          return (
+            <div key={index}> 
+              <p>
+                <strong>Definition: </strong>{definition.definition}
+                <br />
+                <em>{definition.example}</em>
+                <br />
+                <Synonyms synonyms ={definition.synonyms} />
+              </p>
+            </div>
+          );
+        })}
     </div>
   );
 }
