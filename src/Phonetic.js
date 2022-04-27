df1714f62c69e413a9843de6daf402952b74aa3d
anatick <91561543+anatick@users.noreply.github.com>
2022-04-27 15:36:38 +0100

    changed link - building website failed

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -7,7 +7,7 @@ export default function Phonetic (props) {
  return (
    <div className="Phonetic">
       {props.phonetic.text}
-      <a href={props.phonetic.audio} target ="_blank" rel="noopener">
+      <a href={props.phonetic.audio} target ="_blank" rel="noopener noreferrer"> 
       {"  "} 🔊
      </a>
    </div>
